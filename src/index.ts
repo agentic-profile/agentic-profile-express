323466e0eee76844d5ae64f66f477644f61c07db
Mike Prince <mike@mikeprince.com>
2025-03-02 19:41:17 -0800

    Export handleAgentChatMessage

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export { default as app } from "./app.js";
 export { storage, setStorage } from "./storage/handle.js";
 export { InMemoryStorage } from "./storage/memory.js";
+export { handleAgentChatMessage } from "./chat/full.js"
 
 export * from "./routes.js";
-
 export * from "./storage/models.js";
 export * from "./chat/models.js";
 export * from "./ai-providers/models.js";
